422617dffbc7bf4fce99c82d247f8e78e63d90c5
Kevin Kenfack <kenfackjoelkevin@yahoo.com>
2024-10-30 18:02:14 +0100

    update content

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -3,14 +3,31 @@ import Link from "next/link";
 export const Footer = () => {
   return (
     <footer className="mx-auto w-full max-w-5xl text-white/80 p-6">
-      <span>Built by </span>
-      <Link
-        href="https://kevinkenfack.com"
-        target="_blank"
-        className="transition-colors underline underline-offset-4 hover:text-neutral-200"
-      >
-        Kevin Kenfack ❤️.
-      </Link>
+      <div className="flex justify-between">
+        <span>Built by </span>
+        <Link
+          href="https://kevinkenfack.com"
+          target="_blank"
+          className="transition-colors underline underline-offset-4 hover:text-neutral-200"
+        >
+          Kevin Kenfack ❤️.
+        </Link>
+      </div>
+      <div className="mt-4 flex justify-center space-x-4">
+        <Link
+          href="/terms"
+          className="transition-colors underline underline-offset-4 hover:text-neutral-200"
+        >
+          Terms and Conditions
+        </Link>
+        <span>|</span>
+        <Link
+          href="/privacy"
+          className="transition-colors underline underline-offset-4 hover:text-neutral-200"
+        >
+          Privacy Policy
+        </Link>
+      </div>
     </footer>
   );
 };
