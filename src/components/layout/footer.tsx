38dc0fa9030d09b0085b1506712b8d208ab2d488
Kevin Kenfack <kenfackjoelkevin@yahoo.com>
2024-10-30 18:29:49 +0100

    update content

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -2,31 +2,32 @@ import Link from "next/link";
 
 export const Footer = () => {
   return (
-    <footer className="mx-auto w-full max-w-5xl text-white/80 p-6">
-      <span>Built by </span>
-      <Link
-        href="https://kevinkenfack.com"
-        target="_blank"
-        className="transition-colors underline underline-offset-4 hover:text-neutral-200"
-      >
-        Kevin Kenfack ❤️.
-      </Link>
-        <br/>
-      <Link
-        href="/privacy"
-        target="_blank"
-        className="transition-colors underline underline-offset-4 hover:text-neutral-200"
-      >
-        Privacy Policy
-      </Link>
-        <br/>
-      <Link
-        href="/terms"
-        target="_blank"
-        className="transition-colors underline underline-offset-4 hover:text-neutral-200"
-      >
-        Terms and Conditions
-      </Link>
+    <footer className="mx-auto w-full max-w-5xl text-white/80 p-6 flex flex-col items-center">
+      <div className="flex justify-between w-full items-center">
+        <span className="mr-2">Built by</span>
+        <Link
+          href="https://kevinkenfack.com"
+          target="_blank"
+          className="transition-colors underline underline-offset-4 hover:text-neutral-200"
+        >
+          Kevin Kenfack ❤️
+        </Link>
+      </div>
+      <div className="mt-4 flex justify-center space-x-2">
+        <Link
+          href="/terms"
+          className="transition-colors underline underline-offset-4 hover:text-neutral-200"
+        >
+          Terms and Conditions
+        </Link>
+        <span className="text-neutral-400">|</span>
+        <Link
+          href="/privacy"
+          className="transition-colors underline underline-offset-4 hover:text-neutral-200"
+        >
+          Privacy Policy
+        </Link>
+      </div>
     </footer>
   );
 };
