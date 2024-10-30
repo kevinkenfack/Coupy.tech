362532bb87f282f486419a1fadc80c12b66b6663
Kevin Kenfack <kenfackjoelkevin@yahoo.com>
2024-10-30 18:45:12 +0100

    update content

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -4,13 +4,12 @@ export const Footer = () => {
   return (
     <footer className="mx-auto w-full max-w-5xl text-white/80 p-6 flex flex-col items-center">
       <div className="flex justify-between w-full items-center">
-        <span className="text-neutral-400">Built by</span>
         <Link
           href="https://kevinkenfack.com"
           target="_blank"
           className="transition-colors underline underline-offset-4 hover:text-neutral-200"
         >
-          Kevin Kenfack ❤️
+           Buid byKevin Kenfack ❤️
         </Link>
         <br/>
       </div>
