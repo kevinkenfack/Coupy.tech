859acb536c44b396b97f068c1b132938f91682bf
Kevin Kenfack <kenfackjoelkevin@yahoo.com>
2024-10-30 13:17:48 +0100

    add footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 export const Footer = () => {
   return (
     <footer className="mx-auto w-full max-w-5xl text-white/80 p-6">
-      {/* <span>Built by </span>{" "}
+      <span>Built by </span>
       <Link
-        href="https://github.com/mehrabmp"
+        href="https://github.com/kevinkenfack"
         target="_blank"
         className="transition-colors underline underline-offset-4 hover:text-neutral-200"
       >
-        Mehrab.
-      </Link> */}
+        Kevin Kenfack.
+      </Link>
     </footer>
   );
 };
