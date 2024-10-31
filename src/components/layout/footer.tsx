048636760e58c0848abd0254cd96d1fdea3f1554
Kevin Kenfack <kenfackjoelkevin@yahoo.com>
2024-10-31 07:07:17 +0100

    change footer style

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -10,7 +10,7 @@ export const Footer = () => {
     <footer className="bg-white dark:bg-[#0a0a0a] text-black dark:text-white py-10">
       <div className="max-w-5xl mx-auto px-4">
         <div className="text-center space-y-4">
-          <span className="text-gray-700 dark:text-gray-300">Built by</span>
+          <span className="text-gray-700 dark:text-gray-300">Built by&nbsp;</span>
           <Link
             href="https://kevinkenfack.com"
             target="_blank"
@@ -34,7 +34,7 @@ export const Footer = () => {
         </div>
 
         <div className="mt-6 border-t border-gray-300 dark:border-gray-700 pt-4 text-center text-sm text-gray-500 dark:text-gray-400">
-          © 2024 Kevin Kenfack. All rights reserved.
+          © 2024 Coupy. All rights reserved.
         </div>
       </div>
     </footer>
